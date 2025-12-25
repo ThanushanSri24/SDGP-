bd2dba460968757de0ea54cf5ee14fa8ac052034
Prashanthy <amalanprashanthy@gmail.com>
2025-12-26 00:21:13 +0530

    Export main functional components

--- Frontend/admin/app/(tabs)/DriverAlert.tsx
+++ Frontend/admin/app/(tabs)/DriverAlert.tsx
@@ -5,4 +5,8 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Clock, TrafficCone, Wrench, Cloud, AlertCircle, CheckCircle,
 } from "lucide-react-native";
 
+export default function AlertScreen() {
+    
+}
+
 
