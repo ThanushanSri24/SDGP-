15bd64ca8c3779cb450a0e73274402751097475c
Prashanthy <amalanprashanthy@gmail.com>
2025-12-26 00:12:47 +0530

    Import StatusBar component to control device's status bar appaerance

--- Frontend/admin/app/(tabs)/DriverAlert.tsx
+++ Frontend/admin/app/(tabs)/DriverAlert.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, TextInput, ScrollView, Alert } from "react-native";
-
+import { StatusBar } from "expo-status-bar";
 
 
