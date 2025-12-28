2eaf4a9857d4362c61a818d0fed7335c392f8881
Hasini Siriwardena <hasinisiriwardena1003@gmail.com>
2025-12-28 11:28:36 +0530

    Update parent alert1

--- Frontend/admin/app/Parent/ParentAlert.tsx
+++ Frontend/admin/app/Parent/ParentAlert.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+export default function AlertsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Emergency Alerts</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  text: { fontSize: 20, color: "red" },
+});
