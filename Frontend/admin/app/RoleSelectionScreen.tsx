336a3b3339684a52ad98f9986d99ce36b3696765
Amoda17 <liyanageamoda@gmail.com>
2025-12-30 10:25:05 +0530

    Added imports skeleton for RoleSelectionScreen

---
+++ Frontend/admin/app/RoleSelectionScreen.tsx
@@ -0,0 +1,10 @@
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import React from "react";
+import {
+    Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+
