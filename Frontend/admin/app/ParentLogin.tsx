ff5b45a8bf26df3e1233d60f25f79f88e00782ce
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-01-01 21:01:42 +0530

    Added the title, email, passwords with styles

--- Frontend/admin/app/ParentLogin.tsx
+++ Frontend/admin/app/ParentLogin.tsx
@@ -19,6 +19,41 @@ export default function LoginScreen() {
       <TouchableOpacity style={styles.backButton}>
         <Text style={styles.backText}>‹</Text>
       </TouchableOpacity>
+
+      <View style={styles.container}>
+        {/* TITLE */}
+        <Text style={styles.title}>
+          Welcome back! Glad{"\n"}to see you, Again!
+        </Text>
+
+        {/* EMAIL */}
+        <TextInput
+          placeholder="Enter your email"
+          placeholderTextColor="#9CA3AF"
+          style={styles.input}
+          keyboardType="email-address"
+        />
+
+        {/* PASSWORD */}
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Enter your password"
+            placeholderTextColor="#9CA3AF"
+            secureTextEntry={!showPassword}
+            style={styles.passwordInput}
+          />
+          <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+          >
+
+            <Ionicons
+              name={showPassword ? "eye-off" : "eye"}
+              size={22}
+              color="#6B7280"
+            /> 
+          </TouchableOpacity>
+        </View>
+       </View>
     </SafeAreaView>
   );
 }
@@ -43,6 +78,42 @@ const styles = StyleSheet.create({
   backText: {
     fontSize: 20,
     fontWeight: "600",
+  },
+
+  container: {
+    paddingHorizontal: 24,
+    marginTop: 30,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: "700",
+    marginBottom: 40,
+    color: "#111827",
+  },
+
+  input: {
+    height: 52,
+    borderWidth: 1,
+    borderColor: "#50bcffff",
+    borderRadius: 12,
+    paddingHorizontal:14,
+    fontSize: 15,
+    marginBottom: 18,
+  },
+
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#50bcffff",
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    height: 52,
+  },
+  passwordInput: {
+    flex: 1,
+    fontSize: 15,
   },
 
 });
