527604a0bdfea1ac4482cd629dd616007ea60ece
Prashanthy <amalanprashanthy@gmail.com>
2026-02-12 23:22:34 +0530

    layout of navigation bar for driver app

--- Frontend/admin/app/Driver/_layout.tsx
+++ Frontend/admin/app/Driver/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from "expo-router";
-import { Map, Users, Megaphone, User } from "lucide-react-native";
+import { Map, Users, Megaphone, User, CreditCard } from "lucide-react-native";
 
 export default function TabLayout() {
   return (
@@ -11,15 +11,30 @@ export default function TabLayout() {
           borderTopWidth: 1,
           borderColor: "#E5E7EB",
           paddingTop: 4,
+          paddingBottom: 8,
+          height: 97,
         },
         tabBarActiveTintColor: "#2563EB",
         tabBarInactiveTintColor: "#6B7280",
         tabBarLabelStyle: {
           fontSize: 12,
           fontWeight: "500",
+          marginTop: 4,
+        },
+        tabBarIconStyle: {
+          marginTop: 4,
         },
       }}
     >
+      <Tabs.Screen
+        name="DriverMap"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color, size }) => (
+            <Map color={color} size={24} />
+          ),
+        }}
+      />
       
       <Tabs.Screen
         name="DriverAlert"
@@ -30,7 +45,27 @@ export default function TabLayout() {
           ),
         }}
       />
+
       
+      <Tabs.Screen
+        name="Payment"
+        options={{
+          title: "Payment",
+          tabBarIcon: ({ color, size }) => (
+            <CreditCard color={color} size={24} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="DriverProfile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <User color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
