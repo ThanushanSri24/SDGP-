f3bc8c107e71b165527cef484e8d6ce67ef3a2d6
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-02-18 23:46:05 +0530

    Add email input and password container layout

--- Frontend/admin/app/DriverLogin.tsx
+++ Frontend/admin/app/DriverLogin.tsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function LoginScreen() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <SafeAreaView style={styles.safe}>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome back! Glad to see you, Again!</Text>
+        
+        <TextInput
+          placeholder="Enter your email"
+          placeholderTextColor="#9CA3AF"
+          style={styles.input}
+          keyboardType="email-address"
+        />
+
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Enter your password"
+            placeholderTextColor="#9CA3AF"
+            secureTextEntry={!showPassword}
+            style={styles.passwordInput}
+          />
+        </View>  
       </View>
     </SafeAreaView>
   );
@@ -25,5 +49,27 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginBottom: 40,
     color: "#111827",
+  },
+  input: {
+    height: 52,
+    borderWidth: 1,
+    borderColor: "#5AA9E6",
+    borderRadius: 12,
+    paddingHorizontal:14,
+    fontSize: 15,
+    marginBottom: 18,
+  },
+   passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#5AA9E6",
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    height: 52,
+  },
+  passwordInput: {
+    flex: 1,
+    fontSize: 15,
   },
 });
