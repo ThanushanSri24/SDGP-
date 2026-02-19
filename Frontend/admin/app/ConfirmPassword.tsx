6e3caeaa0e5bbc677e8a0b9ba0b190679b0eeb82
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-02-19 14:07:06 +0530

    add update button and keyboard avoiding layout

--- Frontend/admin/app/ConfirmPassword.tsx
+++ Frontend/admin/app/ConfirmPassword.tsx
@@ -1,113 +1,127 @@
-import React from "react";
-import { Image } from "react-native";
-import { useState } from "react";
-import { TextInput } from "react-native";
-const [newPassword, setNewPassword] = useState("");
-const [confirmPassword, setConfirmPassword] = useState("");
-import { Ionicons } from "@expo/vector-icons";
-
-
-
+import React, { useState } from "react";
 import {
   SafeAreaView,
   View,
+  ScrollView,
   Text,
-  StyleSheet,
+  TextInput,
   TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Image,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 
-export default function ConfirmPassword() {
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
-  const [showNewPassword, setShowNewPassword] = useState(false);
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [showNewPassword, setShowNewPassword] = useState(false); 
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Confirm Password Screen</Text>
-           source={require("../assets/images/Newpassword.png")}
-      style={styles.logo}
-      resizeMode="contain"
-    /
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            keyboardShouldPersistTaps="handled"
+          >
+    
 
-    <Text style={styles.title}>Recover Password</Text>
+            {/* Logo */}
+            <Image
+              source={require("../assets/images/Newpassword.png")}
+              style={styles.logo}
+              resizeMode="contain"
+            />
 
-    <Text style={styles.subtitle}>
-      Your Identity has been verified{"\n"}
-      Set your new password
-    </Text>
+            {/* Title */}
+            <Text style={styles.title}>Recover Password</Text>
+            <Text style={styles.subtitle}>
+              Your Identity has been verified{"\n"}
+              Set your new password
+            </Text>
 
-    <View style={styles.form}>
+            {/* FORM */}
+            <View style={styles.form}>
+              
+              {/* New Password */}
 
-  <TextInput
-    placeholder="New Password"
-    placeholderTextColor="#9CA3AF"
-    secureTextEntry
-    style={styles.input}
-    value={newPassword}
-    onChangeText={setNewPassword}
-  />
+              <View style={styles.passwordContainer}>
+                <TextInput
+                    placeholder="New Password"
+                    placeholderTextColor="#9CA3AF"
+                    secureTextEntry={!showNewPassword}
+                    style={styles.passwordInput}
+                    value={newPassword}
+                    onChangeText={setNewPassword}
+                    autoCapitalize="none"
+                />
 
-  <TextInput
-    placeholder="Confirm Password"
-    placeholderTextColor="#9CA3AF"
-    secureTextEntry
-    style={styles.input}
-    value={confirmPassword}
-    onChangeText={setConfirmPassword}
-  />
+                <TouchableOpacity
+                    onPress={() => setShowNewPassword(!showNewPassword)}
+                >
+                <Ionicons
+                    name={showNewPassword ? "eye" : "eye-off"}
+                    size={22}
+                    color="#6B7280"
+                />
+                </TouchableOpacity>
+              </View>
 
-  <View style={styles.passwordContainer}>
-  <TextInput
-    placeholder="New Password"
-    placeholderTextColor="#9CA3AF"
-    secureTextEntry={!showNewPassword}
-    style={styles.passwordInput}
-    value={newPassword}
-    onChangeText={setNewPassword}
-  />
+              <TouchableOpacity
+                style={{ alignSelf: "flex-end", marginBottom: 12 }}
+              >
 
-  <TouchableOpacity
-    onPress={() => setShowNewPassword(!showNewPassword)}
-  >
-    <Ionicons
-      name={showNewPassword ? "eye" : "eye-off"}
-      size={22}
-      color="#6B7280"
-    />
-  </TouchableOpacity>
-</View>
+              </TouchableOpacity>
 
-<View style={styles.passwordContainer}>
-  <TextInput
-    placeholder="Confirm Password"
-    placeholderTextColor="#9CA3AF"
-    secureTextEntry={!showConfirmPassword}
-    style={styles.passwordInput}
-    value={confirmPassword}
-    onChangeText={setConfirmPassword}
-  />
+              {/* Confirm Password */}
+              <View style={styles.passwordContainer}>
+                <TextInput
+                    placeholder="Confirm Password"
+                    placeholderTextColor="#9CA3AF"
+                    secureTextEntry={!showConfirmPassword}
+                    style={styles.passwordInput}
+                    value={confirmPassword}
+                    onChangeText={setConfirmPassword}
+                    autoCapitalize="none"
+                />
 
-  <TouchableOpacity
-    onPress={() =>
-      setShowConfirmPassword(!showConfirmPassword)
-    }
-  >
-    <Ionicons
-      name={showConfirmPassword ? "eye" : "eye-off"}
-      size={22}
-      color="#6B7280"
-    />
-  </TouchableOpacity>
-</View>
+                <TouchableOpacity
+                    onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                <Ionicons
+                    name={showConfirmPassword ? "eye" : "eye-off"}
+                    size={22}
+                    color="#6B7280"
+                />
+                </TouchableOpacity>
+              </View>
 
+              <TouchableOpacity
+                style={{ alignSelf: "flex-end", marginBottom: 35 }}
+              >
+ 
+              </TouchableOpacity>
 
-</View>
-
-
-
-      </View>
+              {/* Login Button */}
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Update</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
@@ -115,63 +129,82 @@ export default function ConfirmPassword() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
   },
 
   logo: {
-  width: 200,
-  height: 200,
-  alignSelf: "center",
-  marginBottom: 10,
-},
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
 
-title: {
-  fontSize: 25,
-  fontWeight: "700",
-  textAlign: "center",
-  marginBottom: 6,
-  color: "#000",
-},
+  title: {
+    fontSize: 25,
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 6,
+    color: "#000",
+  },
 
-subtitle: {
-  fontSize: 14,
-  color: "#555",
-  textAlign: "center",
-  marginBottom: 30,
-},
+  subtitle: {
+    fontSize: 14,
+    color: "#555",
+    textAlign: "center",
+    marginBottom: 30,
+  },
 
-form: {
-  width: "100%",
-},
+  form: {
+    width: "100%",
+    
+  },
 
-input: {
-  height: 52,
-  borderWidth: 1,
-  borderColor: "#50bcff",
-  borderRadius: 12,
-  paddingHorizontal: 14,
-  fontSize: 15,
-  marginBottom: 18,
-},
+  label: {
+    fontSize: 13,
+    fontWeight: "600",
+    color: "#333",
+    marginBottom: 6,
+  },
 
-passwordContainer: {
-  flexDirection: "row",
-  alignItems: "center",
-  borderWidth: 1,
-  borderColor: "#50bcff",
-  borderRadius: 12,
-  paddingHorizontal: 14,
-  height: 52,
-  marginBottom: 18,
-},
+  input: {
+    height: 52,
+    borderWidth: 1,
+    borderColor: "#50bcff",
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    fontSize: 15,
+    marginBottom: 18,
+  },
 
-passwordInput: {
-  flex: 1,
-  fontSize: 15,
-},
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#50bcff",
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    height: 52,
+  },
 
+  passwordInput: {
+    flex: 1,
+    fontSize: 15,
+  },
 
+  button: {
+    backgroundColor: "#50bcff",
+    height: 50,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: -5,
+  },
+
+  buttonText: {
+    color: "#0b0909",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 
 });
