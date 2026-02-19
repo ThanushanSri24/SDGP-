9717ad393928ffda96dd8b6e11fb6f95274dff73
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-02-19 12:20:43 +0530

    add forget password illustration and headings

--- Frontend/admin/app/SendMail.tsx
+++ Frontend/admin/app/SendMail.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Image } from "react-native";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 export default function ForgetPasswordScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Forget Password Screen</Text>
+
+      <Image
+  source={require("../assets/images/Sendmail.png")}
+  style={styles.image}
+  resizeMode="contain"
+/>
+
+<Text style={styles.title}>FORGET PASSWORD!</Text>
+
+<Text style={styles.subtitle}>
+  Provide your account's email for which you{"\n"}
+  want to reset your password.
+</Text>
     </SafeAreaView>
   );
 }
@@ -15,4 +29,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+
+  image: {
+  width: 210,
+  height: 200,
+  marginTop: 80,
+  marginBottom: 20,
+},
+
+title: {
+  fontSize: 22,
+  fontWeight: "bold",
+  textAlign: "center",
+  marginBottom: 10,
+},
+
+subtitle: {
+  textAlign: "center",
+  color: "gray",
+  fontSize: 13,
+  marginBottom: 30,
+},
+
 });
