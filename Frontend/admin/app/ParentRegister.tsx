d27a09e1213837128005ae75a8805942dfef9a41
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-02-19 10:50:12 +0530

    Add back button and form container layout

--- Frontend/admin/app/ParentRegister.tsx
+++ Frontend/admin/app/ParentRegister.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useRouter } from "expo-router";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -6,23 +7,44 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+
+type IconName = keyof typeof MaterialCommunityIcons.glyphMap;
+
 export default function ParentRegisterScreen() {
+  const scrollRef = useRef<ScrollView | null>(null);
+  const router = useRouter();
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
         style={{ flex: 1 }}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 20}
       >
-        <ScrollView contentContainerStyle={styles.scroll}>
+        <ScrollView
+          ref={scrollRef}
+          contentContainerStyle={styles.scroll}
+          keyboardShouldPersistTaps="handled"
+          showsVerticalScrollIndicator={false}
+        >
+    
+        <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={24} color="#111827" />
+        </TouchableOpacity>
+
           <View style={styles.header}>
             <Text style={styles.title}>Parent Portal</Text>
             <Text style={styles.subtitle}>
               Track your child’s school van in real time
             </Text>
           </View>
+
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -34,22 +56,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#e8f8ffff",
   },
+
+  backText: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+
   scroll: {
     padding: 20,
+    flexGrow: 1,
+    justifyContent: "space-between",
+    paddingBottom: 40,
   },
+
   header: {
     alignItems: "center",
     marginBottom: 20,
   },
+
   title: {
     fontSize: 22,
     fontWeight: "700",
     color: "#222",
+    marginTop: 20, 
   },
+
   subtitle: {
     fontSize: 13,
     color: "#666",
     textAlign: "center",
     marginTop: 5,
   },
+
+  formWrapper: {
+    flex: 1,
+  },
+
 });
