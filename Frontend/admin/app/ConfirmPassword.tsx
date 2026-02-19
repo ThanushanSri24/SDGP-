bf2ed642f41c9ed5f51806a7bd35e95efb3aab6a
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-02-19 13:58:40 +0530

    add recover password illustration and headings

--- Frontend/admin/app/ConfirmPassword.tsx
+++ Frontend/admin/app/ConfirmPassword.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Image } from "react-native";
 import {
   SafeAreaView,
   View,
@@ -11,6 +12,17 @@ export default function ConfirmPassword() {
     <SafeAreaView style={styles.container}>
       <View>
         <Text>Confirm Password Screen</Text>
+           source={require("../assets/images/Newpassword.png")}
+      style={styles.logo}
+      resizeMode="contain"
+    /
+
+    <Text style={styles.title}>Recover Password</Text>
+
+    <Text style={styles.subtitle}>
+      Your Identity has been verified{"\n"}
+      Set your new password
+    </Text>
       </View>
     </SafeAreaView>
   );
@@ -23,4 +35,27 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+
+  logo: {
+  width: 200,
+  height: 200,
+  alignSelf: "center",
+  marginBottom: 10,
+},
+
+title: {
+  fontSize: 25,
+  fontWeight: "700",
+  textAlign: "center",
+  marginBottom: 6,
+  color: "#000",
+},
+
+subtitle: {
+  fontSize: 14,
+  color: "#555",
+  textAlign: "center",
+  marginBottom: 30,
+},
+
 });
