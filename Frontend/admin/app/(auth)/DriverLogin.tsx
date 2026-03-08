f33e4b14aac5b7c276e3347d1f2dd5f1cd2c6a98
amoda20240818-tech <amoda.20240818@iit.ac.lk>
2026-03-08 15:34:10 +0530

    Driver login logic and email input

--- Frontend/admin/app/(auth)/DriverLogin.tsx
+++ Frontend/admin/app/(auth)/DriverLogin.tsx
@@ -24,5 +24,84 @@ export default function LoginScreen() {
       alert("Please enter both email and password");
       return;
     }
-  }
+
+        try {
+      //await login(email, password, 'driver');
+      router.replace('/Driver');
+    } catch (error: any) {
+      alert(error.message || "Login failed");
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.safe}>
+
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+        <Text style={styles.backText}>‹</Text>
+      </TouchableOpacity>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Welcome back! Glad{"\n"}to see you, Again!
+        </Text>
+
+        <TextInput
+          placeholder="Enter your email"
+          placeholderTextColor="#9CA3AF"
+          style={styles.input}
+          keyboardType="email-address"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+
+  backButton: {
+    marginLeft: 23,
+    marginTop: 40,
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: "#E5E7EB",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  backText: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+
+  container: {
+    paddingHorizontal: 24,
+    marginTop: 30,
+  },
+
+  title: {
+    fontSize: 26,
+    fontWeight: "700",
+    marginBottom: 40,
+    color: "#111827",
+  },
+
+  input: {
+    height: 52,
+    borderWidth: 1,
+    borderColor: "#5AA9E6",
+    borderRadius: 12,
+    paddingHorizontal: 14,
+    fontSize: 15,
+    marginBottom: 18,
+  },
+
+});
